9ee88311919b812cf0de6baf24a758574e57e981
anhmuoi10 <muoi@duali.com>
2022-10-15 23:30:51 +0700

    abc

--- generate-data.js
+++ generate-data.js
@@ -1,73 +1,91 @@
-const faker = require("faker");
-const fs = require("fs");
+const faker = require('faker');
+const fs = require('fs');
 
 // Set locale to use Vietnamese
-faker.locale = "vi";
+faker.locale = 'vi';
 
-const randomCategoryList = (n) => {
+const randomOrder = (n) => {
   if (n <= 0) return [];
 
-  const categoryList = [];
+  const order = [];
 
   // loop and push category
   Array.from(new Array(n)).forEach(() => {
-    const category = {
+    const od = {
       id: faker.random.uuid(),
-      name: faker.commerce.department(),
+      room_id: Math.floor(Math.random() * 12),
+      customer: faker.name.firstName(),
+      customer_id: faker.random.number(),
+      sale: faker.name.firstName(),
+      in_expected: faker.date.between('2022-06-24T00:00:00.000Z', '2022-06-29T00:00:00.000Z'),
+      out_expected: faker.date.between('2022-07-02T00:00:00.000Z', '2022-07-10T00:00:00.000Z'),
+      type: '100000',
+      discount: `${Math.floor(Math.random() * 20)}%`,
+      phone_customer: faker.phone.phoneNumber(),
+      in_reality: faker.date.between('2022-06-27T00:00:00.000Z', '2022-06-30T00:00:00.000Z'),
+      out_reality: faker.date.between('2022-07-02T00:00:00.000Z', '2022-07-15T00:00:00.000Z'),
+      type_booking: 'Online',
+      total_price: '250000000',
+      additional_price: [
+        {
+          info: 'coca',
+          price: '10000',
+        },
+        {
+          info: 'bread',
+          price: '20000',
+        },
+      ],
       createdAt: Date.now(),
-      updatedAt: Date.now(),
     };
 
-    categoryList.push(category);
+    order.push(od);
   });
 
-  return categoryList;
+  return order;
 };
 
-const randomProductList = (categoryList, numberOfProducts) => {
-  if (numberOfProducts <= 0) return [];
+// const randomProductList = (order, numberOfProducts) => {
+//   if (numberOfProducts <= 0) return [];
 
-  const productList = [];
+//   const productList = [];
 
-  // random data
-  for (const category of categoryList) {
-    Array.from(new Array(numberOfProducts)).forEach(() => {
-      const product = {
-        categoryId: category.id,
-        id: faker.random.uuid(),
-        name: faker.commerce.productName(),
-        color: faker.commerce.color(),
-        price: Number.parseFloat(faker.commerce.price()),
-        description: faker.commerce.productDescription(),
-        createdAt: Date.now(),
-        updatedAt: Date.now(),
-        thumbnailUrl: faker.image.imageUrl(400, 400),
-      };
+//   // random data
+//   for (const category of order) {
+//     Array.from(new Array(numberOfProducts)).forEach(() => {
+//       const product = {
+//         categoryId: category.id,
+//         id: faker.random.uuid(),
+//         name: faker.commerce.productName(),
+//         color: faker.commerce.color(),
+//         price: Number.parseFloat(faker.commerce.price()),
+//         description: faker.commerce.productDescription(),
+//         createdAt: Date.now(),
+//         updatedAt: Date.now(),
+//         thumbnailUrl: faker.image.imageUrl(400, 400),
+//       };
 
-      productList.push(product);
-    });
-  }
+//       productList.push(product);
+//     });
+//   }
 
-  return productList;
-};
+//   return productList;
+// };
 
 // IFFE
 (() => {
   // random data
-  const categoryList = randomCategoryList(4);
-  const productList = randomProductList(categoryList, 5);
+  const order = randomOrder(16);
+  // const productList = randomProductList(order, 5);
 
   // prepare db object
   const db = {
-    categories: categoryList,
-    products: productList,
-    profile: {
-      name: "Po",
-    },
+    order: order,
+  
   };
 
   // write db object to db.json
-  fs.writeFile("db.json", JSON.stringify(db), () => {
-    console.log("Generate data successfully =))");
+  fs.writeFile('order.json', JSON.stringify(db), () => {
+    console.log('Generate data successfully =))');
   });
 })();
--- main.js
+++ main.js
@@ -1,15 +1,159 @@
 const jsonServer = require('json-server');
 const queryString = require('query-string');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const fs = require('fs');
+
+const products = require('./data/products.json');
+const users = require('./data/users.json');
+const order = require('./data/order.json');
+const room = require('./data/room.json');
+
+const data = {
+  ...products,
+  ...users,
+  ...order,
+  ...room,
+};
+
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router(data);
+const userdb = JSON.parse(fs.readFileSync('./data/users.json', 'UTF-8'));
+const orderdb = JSON.parse(fs.readFileSync('./data/order.json', 'UTF-8'));
 const middlewares = jsonServer.defaults();
+
+const SECRET_KEY = '123456789';
+const expiresIn = '5d';
 
 // Set default middlewares (logger, static, cors and no-cache)
 server.use(middlewares);
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
 
 // Add custom routes before JSON Server router
 server.get('/echo', (req, res) => {
   res.jsonp(req.query);
+});
+
+// Create a token from a payload
+function createToken(payload) {
+  return jwt.sign(payload, SECRET_KEY, { expiresIn });
+}
+
+// Verify the token
+function verifyToken(token) {
+  return jwt.verify(token, SECRET_KEY, (err, decode) => (decode !== undefined ? decode : err));
+}
+
+// Check if the user exists in database
+function isAuthenticated({ email, password }) {
+  return (
+    userdb.users.findIndex((user) => user.email === email && user.password === password) !== -1
+  );
+}
+
+// Register New User
+server.post('/auth/register', (req, res) => {
+  console.log('register endpoint called; request body:');
+  console.log(req.body);
+  const { email, password, name, phone, type, location } = req.body;
+
+  if (isAuthenticated({ email, password }) === true) {
+    const status = 401;
+    const message = 'Email and Password already exist';
+    res.status(status).json({ status, message });
+    return;
+  }
+
+  fs.readFile('./data/users.json', (err, data) => {
+    if (err) {
+      const status = 401;
+      const message = err;
+      res.status(status).json({ status, message });
+      return;
+    }
+
+    // Get current users data
+    var data = JSON.parse(data.toString());
+
+    // Get the id of last user
+    var last_item_id = data.users[data.users.length - 1].id;
+
+    //Add new user
+    data.users.push({
+      id: last_item_id + 1,
+      email: email,
+      password: password,
+      name: name,
+      phone: phone,
+      type: type,
+      location: location,
+    }); //add some data
+    var writeData = fs.writeFile('./data/users.json', JSON.stringify(data), (err, result) => {
+      // WRITE
+      if (err) {
+        const status = 401;
+        const message = err;
+        res.status(status).json({ status, message });
+        return;
+      }
+    });
+  });
+
+  // Create token for new user
+  const access_token = createToken({ email, password });
+  console.log('Access Token:' + access_token);
+  res.status(200).json({ access_token });
+});
+
+// Login to one of the users from ./users.json
+server.post('/auth/login', (req, res) => {
+  console.log('login endpoint called; request body:');
+  console.log(req.body);
+  const { email, password } = req.body;
+  if (isAuthenticated({ email, password }) === false) {
+    const status = 401;
+    const message = 'Incorrect email or password';
+    res.status(status).json({ status, message });
+    return;
+  }
+  const access_token = createToken({ email, password });
+
+  // find user
+  const user = userdb.users.find((user) =>
+    user.email === email && user.password === password ? user : null
+  );
+
+  console.log('Access Token:' + access_token);
+  res.status(200).json({ access_token, user });
+});
+
+server.use(/^(?!\/auth).*$/, (req, res, next) => {
+  if (
+    req.headers.authorization === undefined ||
+    req.headers.authorization.split(' ')[0] !== 'Bearer'
+  ) {
+    const status = 401;
+    const message = 'Error in authorization format';
+    res.status(status).json({ status, message });
+    return;
+  }
+  try {
+    let verifyTokenResult;
+    verifyTokenResult = verifyToken(req.headers.authorization.split(' ')[1]);
+
+    if (verifyTokenResult instanceof Error) {
+      const status = 401;
+      const message = 'Access token not provided';
+      res.status(status).json({ status, message });
+      return;
+    }
+    next();
+  } catch (err) {
+    const status = 401;
+    const message = 'Error access_token is revoked';
+    res.status(status).json({ status, message });
+  }
 });
 
 // To handle POST, PUT and PATCH you need to use a body-parser
@@ -27,6 +171,15 @@ server.use((req, res, next) => {
   next();
 });
 
+// api return total price of order
+server.get('/api/order/totalPrice', (req, res) => {
+  const total_price = orderdb.order.reduce((acc, curr) => {
+    return acc + Number(curr.total_price);
+  }, 0);
+  console.log('total_price: ' + total_price);
+  res.status(200).json({ total_price });
+});
+
 // Custom output for LIST with pagination
 router.render = (req, res) => {
   // Check GET with pagination
@@ -40,9 +193,9 @@ router.render = (req, res) => {
     const result = {
       data: res.locals.data,
       pagination: {
-        _page: Number.parseInt(queryParams._page) || 1,
-        _limit: Number.parseInt(queryParams._limit) || 10,
-        _totalRows: Number.parseInt(totalCountHeader),
+        page: Number.parseInt(queryParams._page) || 1,
+        limit: Number.parseInt(queryParams._limit) || 10,
+        total: Number.parseInt(totalCountHeader),
       },
     };
 
